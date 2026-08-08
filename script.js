6efd50f8a4c3d4b59c5051f4ef0ca9d06f9f0bfa
russom <russom.g.meskel@gmail.cpm>
2026-08-08 21:07:25 +0100

    refactor: reorganised episode card

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 //You can edit ALL of the code here
 
-
 function setup() {
   const tvShows = getAllEpisodes();
+
   const tvShowCards = tvShows.map(createTvShowCard);
-  
+
   root.append(...tvShowCards);
 }
 
 function createTvShowCard(tvShow) {
   const tvShowCard = document
     .getElementById("tv-show-card")
-    .content.cloneNode(true);
+    .content
+    .cloneNode(true);
 
-  tvShowCard.querySelector("h3").textContent = `${tvShow.name} - S${String(
-    tvShow.season,
-  ).padStart(2, "0")}E${String(tvShow.number).padStart(2, "0")}`;
-  
+  const title = tvShowCard.querySelector("h3");
+  title.textContent = `${tvShow.name} - S${String(tvShow.season).padStart(
+    2,
+    "0",
+  )}E${String(tvShow.number).padStart(2, "0")}`;
+
   const image = tvShowCard.querySelector("img");
   image.src = tvShow.image.medium;
   image.alt = tvShow.name;
-  tvShowCard.querySelector("p").innerHTML = tvShow.summary;
+
+  const summary = tvShowCard.querySelector("p");
+  summary.innerHTML = tvShow.summary;
+
+  const link = tvShowCard.querySelector("a");
+  link.href = tvShow.url;
+  link.textContent = "Source";
+  link.target = "_blank";
 
   return tvShowCard;
 }
